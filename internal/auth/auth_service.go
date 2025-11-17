beeda51f9963717baf59d19d5b0648f02f673a96
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-17 21:36:11 +0700

    adds login feature

--- internal/auth/auth_service.go
+++ internal/auth/auth_service.go
@@ -1,1 +1,28 @@
 package auth
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+func GenerateJWT(userID string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+
+	claims := &jwt.RegisteredClaims{
+		Subject:   userID,
+		ExpiresAt: jwt.NewNumericDate(expirationTime),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtSecretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
