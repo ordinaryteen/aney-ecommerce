71e4ad9e18e776779031f6153acef0858d8e0e6f
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-18 20:01:36 +0700

    integrate themes and navbar to app.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Flex, Heading, Button, Spacer } from '@chakra-ui/react';
+
+const Navbar = () => {
+  return (
+    <Box bg="dark" px={4} py={4} borderBottom="1px" borderColor="whiteAlpha.200" position="sticky" top={0} zIndex={10}>
+      <Flex alignItems="center" maxW="1200px" mx="auto">
+        <Heading size="lg" color="brand.500" letterSpacing="tight">
+          feez.
+        </Heading>
+
+        <Spacer />
+
+        <Flex gap={4}>
+          <Button variant="ghost" color="gray.300" _hover={{ bg: 'whiteAlpha.200', color: 'white' }}>
+            Login
+          </Button>
+          <Button bg="brand.500" color="white" _hover={{ bg: 'brand.600' }}>
+            Sign Up
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
